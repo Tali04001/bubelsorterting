d845e92c51ab5a974a604f9a5f0878fda8015462
Tali04001 <89461076+Tali04001@users.noreply.github.com>
2022-02-11 14:59:36 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Imorgon_bubelsort
+{
+    class Program
+    {
+        
+     static void BubbleSort(int[] Imorgon)
+        {
+            bool needsSorting = true;
+            for (int i = 0; i < Imorgon.Length - 1 && needsSorting; i++)
+            {
+                needsSorting = false;
+                for (int j = 0; j < Imorgon.Length - 1 - i; j++)
+                {
+                    if (Imorgon[j] > Imorgon[j + 1])
+                    {
+                        needsSorting = true;
+                        int tmp = Imorgon[j +1];
+                        Imorgon[j + 1] = Imorgon[j];
+                        Imorgon[j] = tmp;
+                    }
+                }
+ 
+            }
+        }
+ 
+        static int[] GenerateImorgon(int size)
+        {
+            Random rnd = new Random();
+            int[] Imorgon = new int[size];
+ 
+            for (int i = 0; i < Imorgon.Length; i++)
+                Imorgon[i] = rnd.Next(Imorgon.Length);
+ 
+            return Imorgon;
+        }
+ 
+        static void Main(string[] args)
+        {
+            int[] sizes = new int[] { 1000, 2000, 4000, 8000 };
+ 
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                Console.WriteLine("slumpar saker " + sizes[i]);
+                int[] Imorgon  = GenerateImorgon(sizes[i]);
+                
+                Console.WriteLine("Sorterar slumpis");
+                DateTime startkelb = DateTime.Now;
+                BubbleSort(Imorgon);
+                TimeSpan deltaTid = DateTime.Now - startkelb;
+                Console.WriteLine("Det tog {0:0.00} millisekunder.\n", deltaTid.TotalMilliseconds);
+ 
+            }
+        }
+    }
+}
+ 
